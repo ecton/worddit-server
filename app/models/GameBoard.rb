2e35b25d6e3a15e2b926c68ea050cdda59b3ad4e
Andrew Scala <ascala@ascala-laptop.(none)>
2010-06-15 17:16:39 -0400

    Missing files GameBoard.rb and GameMove.rb

---
+++ app/models/GameBoard.rb
@@ -0,0 +1,22 @@
+class GameBoard < Hash
+  include CouchRest::CastedModel
+  
+  property :rows, :cast_as => ['GameBoardRow']
+
+	def tile_at(row, column)
+		return @rows[row][column]
+	end
+end
+
+class GameBoardRow < Hash
+  include CouchRest::CastedModel
+  
+  property :columns, :cast_as => ['GameBoardSquare']
+end
+
+class GameBoardSquare < Hash
+  include CouchRest::CastedModel
+
+  property :tile, :cast_as => 'GameTile'
+  property :special
+end
