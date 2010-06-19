2a99f3bb4703fa23f8dd1612a380928ebeb084f2
Jonathan Johnson <jon@nilobject.com>
2010-06-19 17:00:13 -0500

    Attempting to fix auth tokens being saved properly.

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,19 +38,19 @@ class User < CouchRest::ExtendedDocument
   end
 
   def check_password(password)
-    User.hash_password(password) == password_hash
+    User.hash_password(password) == self.password_hash
   end
   
   def setup_auth_token(client_type, device_id)
     # lookup an existing auth or create a new one
-    auths ||= []
-    auth = auths.find_all{|a| a.client_type == client_type && a.device_id == device_id}.first
+    self.auths ||= []
+    auth = self.auths.find_all{|a| a.client_type == client_type && a.device_id == device_id}.first
     if auth.nil?
       auth = UserAuthentication.new(:client_type => client_type, :device_id => device_id)
-      auths << auth
+      self.auths << auth
     end
     
-    auth.authentication_token = id + Digest::SHA1.hexdigest((0...8).map{65.+(rand(25)).chr}.join + client_type + device_id)
+    auth.authentication_token = self.id + Digest::SHA1.hexdigest((0...8).map{65.+(rand(25)).chr}.join + client_type + device_id)
     auth.expire_date = Time.now + 30.days
     return auth.authentication_token
   end
