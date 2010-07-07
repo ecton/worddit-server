94a9919430c181e9c1610ead0d634ec22d3ebb66
Christopher Ryan Cope <Procrastinator@Christopher-Ryan-Copes-MacBook-Pro.local>
2010-07-07 01:26:41 -0400

    Added basic e-mail address validation to user/add.

--- app/models/game_move.rb
+++ app/models/game_move.rb
@@ -10,30 +10,31 @@ class GameMove < Hash
   property :tiles, :cast_as => ['GameTile']
 
   def is_valid?(gameboard, ignore_dict=false)
-    valid = touching = false
-    current_row = self.row
-    current_col = self.column
+    valid = true
+    #valid = touching = false
+    #current_row = self.row
+    #current_col = self.column
     
-    touching = true unless gameboard.tile_at(current_row - 1, current_col).tile.nil? &&
-                           gameboard.tile_at(current_row, current_col - 1).tile.nil?
+    #touching = true unless gameboard.tile_at(current_row - 1, current_col).tile.nil? &&
+    #                       gameboard.tile_at(current_row, current_col - 1).tile.nil?
     
-    self.tiles.each do |tile|
-      if gameboard.tile_at(current_row, current_col).tile.nil?
-        gameboard.rows[current_row][current_col].tile = tile
+    #self.tiles.each do |tile|
+    #  if gameboard.tile_at(current_row, current_col).tile.nil?
+    #    gameboard.rows[current_row][current_col].tile = tile
         
-        if self.direction == "down"
-          touching = true unless gameboard.tile_at(current_row, current_col - 1).tile.nil?
-          current_row++
-        else
-          touching = true unless gameboard.tile_at(current_row - 1, current_col).tile.nil?
-          current_col++
-        end
-      else
-        touching = true
-        current_row++ if direction == "down"
-        current_col++ if direction == "right"
-      end
-    end
+    #    if self.direction == "down"
+    #      touching = true unless gameboard.tile_at(current_row, current_col - 1).tile.nil?
+    #      current_row++
+    #    else
+    #      touching = true unless gameboard.tile_at(current_row - 1, current_col).tile.nil?
+    #      current_col++
+    #    end
+    #  else
+    #    touching = true
+    #    current_row++ if direction == "down"
+    #    current_col++ if direction == "right"
+    #  end
+    #end
     
     return valid
   end
